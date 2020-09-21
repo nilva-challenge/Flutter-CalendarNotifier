cc21280014344e0d9bfb15dfcc302ef6e2022bca
alia9906 <abdollahi890@gmail.com>
2020-09-21 10:04:35 -0700

    Squashed commit of the following:
    commit 72c46c3f9b84bebac5ff7408b83da8c8c9d02dc8
    Author: alia9906 <abdollahi890@gmail.com>
    Date:   Mon Sep 21 10:03:49 2020 -0700
    
        project_base is done :sparkles:
    
    commit 7ab317714824571642be106060946e77675942fa
    Author: alia9906 <abdollahi890@gmail.com>
    Date:   Mon Sep 21 09:40:57 2020 -0700
    
        flutter project created

---
+++ lib/core/injection_container.dart
@@ -0,0 +1,9 @@
+import 'package:get_it/get_it.dart';
+
+final sl = GetIt.instance;
+
+Future<void> init() async {
+  // ! Features - Auth Data
+  // ! Core
+  // ! External
+}
---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+main() {
+  runApp(Root());
+}
+
+class Root extends StatelessWidget {
+  const Root({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: null,
+    );
+  }
+}
