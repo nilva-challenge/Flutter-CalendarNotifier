0a89eff64a4666728ce98fa34922ae89a4199b7b
Yousef Roshandel <yousef.r1999@gmail.com>
2020-09-23 20:16:32 +0330

    regenerate Credentials class

--- lib/pages/calendar_page.dart
+++ lib/pages/calendar_page.dart
@@ -3,8 +3,6 @@ import 'package:flutter/widgets.dart';
 import 'package:flutter_calendar_notifier/components/event_list_view.dart';
 import 'package:flutter_calendar_notifier/components/floating_action_button.dart';
 
-//? Google Sign-in
-
 class CalandarPage extends StatefulWidget {
   @override
   State<StatefulWidget> createState() => _CalandarPageState();
