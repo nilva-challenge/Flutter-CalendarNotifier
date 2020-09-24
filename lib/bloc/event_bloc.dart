10b51854df651160eb9b0ce728a9341fd623c75a
Yousef Roshandel <yousef.r1999@gmail.com>
2020-09-24 19:42:35 +0330

    implement states

--- lib/bloc/event_bloc.dart
+++ lib/bloc/event_bloc.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutter_calendar_notifier/data/model/fake_events_model.dart';
 
 part 'event_event.dart';
 part 'event_state.dart';
--- lib/bloc/event_state.dart
+++ lib/bloc/event_state.dart
@@ -2,9 +2,33 @@ part of 'event_bloc.dart';
 
 abstract class EventState extends Equatable {
   const EventState();
-  
+
   @override
   List<Object> get props => [];
 }
 
-class EventInitial extends EventState {}
+class EventInitial extends EventState {
+  const EventInitial();
+}
+
+class EventLoading extends EventState {
+  const EventLoading();
+  @override
+  List<Object> get props => [];
+}
+
+class EventLoaded extends EventState {
+  final FakeEventsModel fakeEventsModel;
+
+  const EventLoaded(this.fakeEventsModel);
+  @override
+  List<Object> get props => [fakeEventsModel];
+}
+
+class EventError extends EventState {
+  final String message;
+
+  const EventError(this.message);
+  @override
+  List<Object> get props => [message];
+}
