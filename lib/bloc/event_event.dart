e9503af885fccfcac872d8270ced940e89ecc259
Yousef Roshandel <yousef.r1999@gmail.com>
2020-09-24 19:33:43 +0330

    implement GetEvents

--- lib/bloc/event_event.dart
+++ lib/bloc/event_event.dart
@@ -2,7 +2,12 @@ part of 'event_bloc.dart';
 
 abstract class EventEvent extends Equatable {
   const EventEvent();
+}
 
+class GetEvents extends EventEvent {
+  final int eventCount;
+
+  GetEvents(this.eventCount);
   @override
-  List<Object> get props => [];
+  List<Object> get props => [eventCount];
 }
