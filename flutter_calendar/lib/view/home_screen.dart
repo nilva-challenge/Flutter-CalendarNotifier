4bc7dc2da70113bc01010c1d85b4cc89044998b2
siavashmirzaeifard <Siavash.mf@gmail.com>
2021-01-14 22:35:53 +0330

    complete application;

--- flutter_calendar/lib/view/home_screen.dart
+++ flutter_calendar/lib/view/home_screen.dart
@@ -7,25 +7,18 @@ Developer siavash
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
-import 'package:googleapis/calendar/v3.dart' as calendar;
-import 'package:googleapis_auth/auth.dart';
-
 import 'add_event_screen.dart';
+import 'package:flutter_calendar/controller/home_controller.dart';
 
+// ignore: must_be_immutable
 class HomeScreen extends StatelessWidget {
 
-  static const _scopes = const [calendar.CalendarApi.CalendarScope];
+HomeController _controller = Get.put(HomeController());
+
   @override
   Widget build(BuildContext context) {
-    var _credentials;
-    if (Platform.isAndroid) {
-      _credentials =  ClientId(
-          "919163830062-ouhqk3pls9ljcih3oh9279b6klelmc7m.apps.googleusercontent.com", "");
-    } else if (Platform.isIOS) {
-      _credentials = ClientId(
-          "YOUR_CLIENT_ID_FOR_IOS_APP_RETRIEVED_FROM_Google_Console_Project_EARLIER",'');
-    }
 
     return Scaffold(
         appBar: AppBar(
@@ -34,38 +27,50 @@ class HomeScreen extends StatelessWidget {
         floatingActionButton: FloatingActionButton(
           child: Icon(Icons.add),
           onPressed: () async {
-            //_addEventController.setStartTime();
             navigator.push(MaterialPageRoute(builder: (context) => AddEventScreen(),));
-            // var datePicked = await DatePicker.showSimpleDatePicker(
-            //    context,
-            //    initialDate: DateTime.now(),
-            //    firstDate: DateTime.now(),
-            //    lastDate: DateTime(2100),
-            //    dateFormat: "dd-MMMM-yyyy",
-            //    locale: DateTimePickerLocale.en_us,
-            //    looping: true,
-            //  );
           },),
         body: SafeArea(
           child: Container(
-            child: ListView.builder(
-              itemCount: 10,
-              itemBuilder: (context, index) {
-                return Padding(
-                  padding: const EdgeInsets.all(2.0),
-                  child: Container(
-                    height: 80,
-                    child: Card(
-                      shadowColor:Colors.black12 ,
-                      elevation: 2,
-                      child: Center(
-                        child: Text(index.toString()),
+            child: GetBuilder(
+              init: _controller,
+              builder: (controller) {
+                return _controller.events.length != 0 ? ListView.builder(
+                  itemCount: _controller.events.length,
+                  itemBuilder: (context, index) {
+                    return Padding(
+                      padding: const EdgeInsets.all(2.0),
+                      child: Container(
+                        height: 80,
+                        child: Card(
+                          shadowColor:Colors.black12 ,
+                          elevation: 2,
+                          child: Center(
+                            child: Text(_controller.events[index].description ?? ''),
+                          ),
+                        ),
                       ),
-                    ),
+                    );
+                  },):Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text('Not Found Any Event'),
+                      SizedBox(height: 20,),
+                      RaisedButton(
+                        child: Text('refresh',style: TextStyle(
+                          color: Colors.white
+                        ),),
+                        color: Colors.blueAccent,
+                        onPressed: () => _controller.getdata(),
+                      )
+                    ],
                   ),
                 );
-              },),
+              },
+            ),
           ),
         ));
   }
+
+
 }
