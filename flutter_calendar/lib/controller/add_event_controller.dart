bb9956ee91e6925f8e575356f44c32c1e2d04137
siavashmirzaeifard <Siavash.mf@gmail.com>
2021-01-14 22:33:35 +0330

    complete insert method event and add that google calendar

--- flutter_calendar/lib/controller/add_event_controller.dart
+++ flutter_calendar/lib/controller/add_event_controller.dart
@@ -4,15 +4,23 @@ Developer sivash
 ~^~^~^~^~^~^~^~^~^*/
 
 
+import 'package:flutter_calendar/util/constanst.dart';
 import 'package:flutter_holo_date_picker/date_picker.dart';
 import 'package:flutter_holo_date_picker/flutter_holo_date_picker.dart';
 import 'package:get/get.dart';
+import 'package:googleapis/calendar/v3.dart';
+import 'package:googleapis_auth/auth.dart';
+import 'package:googleapis_auth/auth_io.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class AddEventController extends GetxController{
+
 
   String startTime = 'Start Time';
   String endTime = 'End Time';
   String title;
+  DatePicker dateStart;
+  DatePicker dateEnd;
 
   setTitle(String value){
     title = value;
@@ -47,5 +55,52 @@ class AddEventController extends GetxController{
     update();
   }
 
+
+  insert() {
+    clientViaUserConsent(Constant.clientID, Constant.scopes, _prompt).then((AuthClient client) {
+      var calendar = CalendarApi(client);
+      calendar.calendarList.list().then((value) => print("VAL $value"));
+
+      String calendarId = "primary";
+      Event event = Event(); // Create object of event
+
+      event.summary = title;
+      DateTime startTime = DateTime.now();
+      DateTime endTime = DateTime.now().add(Duration(days: 1));
+      EventDateTime start = new EventDateTime();
+      start.dateTime = startTime;
+      start.timeZone = "GMT+03:30";
+      event.start = start;
+
+      EventDateTime end = new EventDateTime();
+      end.timeZone = "GMT+03:30";
+      end.dateTime = endTime;
+      event.end = end;
+      try {
+        calendar.events.insert(event, calendarId).then((value) {
+          print("ADD${value.status}");
+          if (value.status == "confirmed") {
+            print('Event added in google calendar');
+          } else {
+            print("Unable to add event in google calendar");
+          }
+        });
+      } catch (e) {
+        print('Error$e');
+      }
+    });
+  }
+
+  void _prompt(String url) async {
+    print("Please go to the following URL and grant access:");
+    print("  => $url");
+    print("");
+
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
 
 }
